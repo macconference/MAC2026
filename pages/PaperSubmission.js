9ee9111730365f8af2109c5d9bc683073baf0136
830Utpal <lakshman2016yadav@gmail.com>
2025-08-06 12:42:13 +0530

    initialised mac2026

--- pages/PaperSubmission.js
+++ pages/PaperSubmission.js
@@ -1,18 +1,10 @@
 import React from "react";
-import dynamic from "next/dynamic";
+import Lottie from "lottie-react";
 import examsPreparation from "../lottieFiles/examsPreparation.json";
 import gradients from "../styles/customGradient.module.css";
-// import SponsorFix from "../components/SponsorFix"; // optional
+import SponsorFix from "../components/SponsorFix";
 
-const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
-
-const Guideline = () => {
-  const [isClient, setIsClient] = React.useState(false);
-
-  React.useEffect(() => {
-    setIsClient(true);
-  }, []);
-
+const guideline = () => {
   return (
     <main
       className={`flex-grow overflow-x-clip bg-gray-50 pt-14 ${gradients.homePage}`}
@@ -24,15 +16,15 @@ const Guideline = () => {
               <h1 className=" text-center text-black text-4xl sm:text-5xl font-bold leading-[3rem] border-b-4 border-blue-500 pb-1 md:pb-3 ">
                 Guidelines
               </h1>
+              {/* <div className="mt-1 sm:mt-4 h-1 w-12 sm:w-20 bg-blue-600 rounded"></div> */}
             </div>
-
-            <section className="p-5 text-gray-600 body-font border-rose-500 rounded mt-4 !ml-0">
-              <div className="flex gap-4 md:flex-row flex-col items-center">
-                <div className="lg:flex-grow md:w-1/2 flex flex-col md:items-start md:text-left mb-2 md:mb-0 items-center text-center">
+            <section className=" p-5  text-gray-600 body-font  border-rose-500 rounded mt-4 !ml-0">
+              <div className=" flex gap-4 md:flex-row flex-col items-center ">
+                <div className=" lg:flex-grow md:w-1/2 flex flex-col md:items-start md:text-left mb-2 md:mb-0 items-center text-center">
                   <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
                     Paper Submission
                   </h1>
-                  <p className="px-6 md:px-0 md:mb-8 leading-relaxed md:font-semibold md:text-lg text-justify">
+                  <p className="px-6 md:px-0 md:mb-8 leading-relaxed  md:font-semibold md:text-lg text-justify">
                     Interested students/ researchers/ academicians/ industry
                     persons can submit their original and previously unpublished
                     work, including specific results. All papers are required to
@@ -48,7 +40,7 @@ const Guideline = () => {
                     <a
                       href="https://www.ieee.org/conferences/publishing/templates.html"
                       target="_blank"
-                      className="font-bold text-blue-500"
+                      className="folt-bold text-blue-500"
                       rel="noreferrer"
                     >
                       {" "}
@@ -60,7 +52,7 @@ const Guideline = () => {
                   <h1 className="title-font sm:text-4xl text-3xl mb-4 mt-8 md:mt-2 font-medium text-gray-900 block">
                     Paper Submission Link
                   </h1>
-                  <p className="px-16 md:px-0 mb-4 leading-relaxed md:font-semibold md:text-lg">
+                  <p className="px-16 md:px-0 mb-4 leading-relaxed  md:font-semibold md:text-lg">
                     All papers must be submitted in PDF via Microsoft CMT:
                   </p>
                   <a
@@ -72,35 +64,37 @@ const Guideline = () => {
                     To be announced soon
                   </a>
                 </div>
-
-                {isClient && (
-                  <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 order-12">
-                    <Lottie animationData={examsPreparation} loop={true} />
-                  </div>
-                )}
+                <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 order-12   ">
+                  <Lottie animationData={examsPreparation} loop={true} />
+                </div>
               </div>
             </section>
-
             <section className="text-gray-600 body-font border-rose-500 rounded !ml-0">
-              <div className="flex p-5 gap-4 md:flex-row flex-col items-center">
-                <div className="lg:flex-grow flex flex-col mb-8 md:mb-0 md:items-start md:text-left items-center text-center">
+              <div className=" flex p-5 gap-4 md:flex-row flex-col items-center">
+                <div className="lg:flex-grow  flex flex-col mb-8 md:mb-0 md:items-start md:text-left items-center text-center">
                   <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 block">
                     Publication
                   </h1>
-                  <p className="mb-4 leading-relaxed md:font-semibold md:text-lg">
+                  <p className="mb-4 leading-relaxed  md:font-semibold md:text-lg">
                     All accepted & presented papers of the Conference by duly
                     registered authors, will be submitted to IEEE Xplore Digital
                     Library for Possible Publication.
                   </p>
+                  {/* <p className="mb-4 leading-relaxed  md:font-semibold md:text-lg">
+                    Extended version of the top 10% papers from MAC 2025 will be
+                    published in a special issue of Defence Science Journal
+                    (SCI).
+                  </p> */}
                 </div>
               </div>
             </section>
           </main>
         </div>
+
         {/* <SponsorFix /> */}
       </div>
     </main>
   );
 };
 
-export default Guideline;
+export default guideline;
